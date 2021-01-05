b16b6b72155e520b80aa97d07699f6f8a72e6a72
Alvaro Menjivar <alvaro1934.c@gmail.com>
2021-01-04 22:35:33 -0600

    Validacion de enlaces de youtube

--- watchAll/views.py
+++ watchAll/views.py
@@ -51,6 +51,7 @@ def eliminar_video_lista(request, video_id, tipo):
     else:
         return redirect('loguearse')
 
+
 def favoritos(request):
     if request.user.is_authenticated:
         videos_publicados = Favoritos.objects.filter(usuario=request.user).get
@@ -97,18 +98,21 @@ def agregar_recurso(request):
         if request.method == 'POST':
             form = RecursoForm(request.POST)
             if form.is_valid():
-                nuevo_Recurso = Video(
-                    nombre=form.cleaned_data.get('nombre'),
-                    fechaPublicacion=form.cleaned_data.get('fechaPublicacion'),
-                    categoria=form.cleaned_data.get('categoria'),
-                    palabraClave=form.cleaned_data.get('palabraClave'),
-                )
-                nuevo_Recurso = form.save(commit=False)
-                nuevo_Recurso.usuario = Cuenta.objects.get(
-                    id=request.user.id)
-                nuevo_Recurso.recurso = str(form.cleaned_data.get('recurso')).split("=")[1]
-                nuevo_Recurso.save()
-                return redirect('inicio')
+                if "www.youtube.com/watch?v" in form.cleaned_data.get('recurso'):
+                    nuevo_Recurso = Video(
+                        nombre=form.cleaned_data.get('nombre'),
+                        fechaPublicacion=form.cleaned_data.get('fechaPublicacion'),
+                        categoria=form.cleaned_data.get('categoria'),
+                        palabraClave=form.cleaned_data.get('palabraClave'),
+                    )
+                    nuevo_Recurso = form.save(commit=False)
+                    nuevo_Recurso.usuario = Cuenta.objects.get(
+                        id=request.user.id)
+                    nuevo_Recurso.recurso = str(form.cleaned_data.get('recurso')).split("=")[1]
+                    nuevo_Recurso.save()
+                    return redirect('inicio')
+                else:
+                    return redirect('favoritos')
         context = {'form': form}
         return render(request, 'agregarRecurso.html', context)
 
@@ -126,6 +130,13 @@ class VideosPersonales(ListView):
 
 class EditarVideo(UpdateView):
     model = Video
+    form_class = RecursoForm
+    template_name = 'agregarRecurso.html'
+    success_url = reverse_lazy('inicio')
+
+
+class EditarCuenta(UpdateView):
+    model = Cuenta
     form_class = RecursoForm
     template_name = 'agregarRecurso.html'
     success_url = reverse_lazy('inicio')
