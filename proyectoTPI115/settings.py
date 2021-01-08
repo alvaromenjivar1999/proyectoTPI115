1a22bd18ddaba19cbd0c9b998490dc610264d095
gonzalo <gondopalacios@gmail.com>
2021-01-07 23:30:23 -0600

    inicial3

--- proyectoTPI115/settings.py
+++ proyectoTPI115/settings.py
@@ -79,6 +79,17 @@ WSGI_APPLICATION = 'proyectoTPI115.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',  # Or path to database file if using sqlite3.
+        'USER': '',  # Not used with sqlite3.
+        'PASSWORD': '',  # Not used with sqlite3.
+        'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
 DATABASES['default'] = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
